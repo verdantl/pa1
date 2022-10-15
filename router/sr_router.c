f37eafe49fba9d9f0b33b64387c4dbbb2e7dfd78
tamanoir0211 <tamanoiryota@gmail.com>
2022-10-15 19:16:18 -0400

    modification

--- router/sr_router.c
+++ router/sr_router.c
@@ -102,7 +102,7 @@ void sr_handle_ip_packet(struct sr_instance* sr,
         /* checking ICMP checkksum */
         uint16_t icmp_sum_temp = icmp_hdr->icmp_sum;
         icmp_hdr->icmp_sum = 0;
-        uint16_t recalculation = cksum(icmp_hdr, sizeof(sr_icmp_hdr_t));
+        uint16_t recalculation = cksum(icmp_hdr, sizeof(packet));
         if (icmp_sum_temp != cksum(icmp_hdr, sizeof(sr_icmp_hdr_t))) {
           icmp_hdr->icmp_sum = icmp_sum_temp;
           printf("%d\n", icmp_sum_temp);
