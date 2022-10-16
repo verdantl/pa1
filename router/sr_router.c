a60e5807c81c20c5ee846a89668fcfce7cec93d1
None <None>
2022-10-15 22:54:47 -0700

    verfying minimum length

--- router/sr_router.c
+++ router/sr_router.c
@@ -83,7 +83,6 @@ void sr_handle_ip_packet(struct sr_instance* sr,
         char* interface/* lent */)
 {
   struct sr_if* received_interface = sr_get_interface(sr, interface);
-  /*sr_ip_hdr_t *iphdr = (sr_ip_hdr_t *)(packet); */
   sr_ethernet_hdr_t *ehdr = (sr_ethernet_hdr_t *)packet;
   sr_ip_hdr_t *iphdr = (sr_ip_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t));
   struct sr_if *find_iterator = sr->if_list;
@@ -95,45 +94,62 @@ void sr_handle_ip_packet(struct sr_instance* sr,
   printf("Below is the destination ip address\n");
   print_addr_ip_int(ntohl(iphdr->ip_dst));
 
+  /* check if IP packet meets minimum length */
+  if (len < sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t)) {
+      printf("IP packet doesn't meet minimum length\n");
+      return;
+  }
+    
+  /* verify checksum of IP packet */
+  uint16_t original_ip_checksum = iphdr->ip_sum;
+  iphdr->ip_sum = 0;
+  if (original_ip_checksum != cksum(iphdr, sizeof(sr_ip_hdr_t))){
+      printf("Checksum of IP packet is inccorect\n");
+      iphdr->ip_sum = original_ip_checksum;
+      return;
+  }
+  iphdr->ip_sum = original_ip_checksum;
+
+  /* find out if packet is for me */
   while (find_iterator) {
     if (find_iterator->ip == iphdr->ip_dst) {
       printf("This is for me!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
       sr_icmp_hdr_t *icmp_hdr = (sr_icmp_hdr_t *)(packet + sizeof(sr_ethernet_hdr_t) + sizeof(sr_ip_hdr_t));
-        /* checking ICMP checkksum */
-        uint16_t icmp_sum_temp = icmp_hdr->icmp_sum;
+
+        /* verify checksum of ICMP packet */
+        uint16_t original_icmp_checksum = icmp_hdr->icmp_sum;
         icmp_hdr->icmp_sum = 0;
-        uint16_t recalculation = cksum(icmp_hdr, len - sizeof(sr_ethernet_hdr_t) - sizeof(sr_ip_hdr_t));
-        printf("%d\n", icmp_sum_temp);
-        printf("%d\n", recalculation);
-        if (icmp_sum_temp != cksum(icmp_hdr, len - sizeof(sr_ethernet_hdr_t) - sizeof(sr_ip_hdr_t))) {
-          icmp_hdr->icmp_sum = icmp_sum_temp;
-          printf("%d\n", icmp_sum_temp);
-          printf("%d\n", recalculation);
-          printf("ICMP header checksum is incorrect\n");
+        if (original_icmp_checksum != cksum(icmp_hdr, len - sizeof(sr_ethernet_hdr_t) - sizeof(sr_ip_hdr_t))) {
+          icmp_hdr->icmp_sum = original_icmp_checksum;
+          printf("Checksum of ICMP packet is incorrect\n");
           return;
         }
-        icmp_hdr->icmp_sum = icmp_sum_temp;
+        icmp_hdr->icmp_sum = original_icmp_checksum;
 
       /* icmp request */
-      if (icmp_hdr->icmp_type == 8) {
+      if (iphdr->ip_p == ip_protocol_icmp) {
 
-          /* change ETHERNET header*/
-          memcpy(ehdr->ether_dhost, ehdr->ether_shost, ETHER_ADDR_LEN);
-          memcpy(ehdr->ether_shost, received_interface->addr, ETHER_ADDR_LEN);
+        /* echo request */
+        if (icmp_hdr->icmp_type == 8) {
 
-          /* change IP header */
-          iphdr->ip_dst = iphdr->ip_src;
-          iphdr->ip_src = received_interface->ip;
+            /* change ETHERNET header*/
+            memcpy(ehdr->ether_dhost, ehdr->ether_shost, ETHER_ADDR_LEN);
+            memcpy(ehdr->ether_shost, received_interface->addr, ETHER_ADDR_LEN);
 
-          /* change ICMP header */
-          icmp_hdr->icmp_type = 0x00;
-          icmp_hdr->icmp_sum = 0;
-          icmp_hdr->icmp_sum = cksum(icmp_hdr, len - sizeof(sr_ethernet_hdr_t) - sizeof(sr_ip_hdr_t));
-          printf("YEAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
-          printf("%d\n", icmp_hdr->icmp_sum);
+            /* change IP header */
+            iphdr->ip_dst = iphdr->ip_src;
+            iphdr->ip_src = received_interface->ip;
 
-          /* send icmp reply back */
-          sr_send_packet(sr, packet, len, interface);
+            /* change ICMP header */
+            icmp_hdr->icmp_type = 0x00;
+            icmp_hdr->icmp_sum = 0;
+            icmp_hdr->icmp_sum = cksum(icmp_hdr, len - sizeof(sr_ethernet_hdr_t) - sizeof(sr_ip_hdr_t));
+            printf("YEAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
+            printf("%d\n", icmp_hdr->icmp_sum);
+
+            /* send icmp reply back */
+            sr_send_packet(sr, packet, len, interface);
+        }
       }
 
     }
