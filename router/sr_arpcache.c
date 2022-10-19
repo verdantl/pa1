2fbaa21fd7ff18dde3e9b58ad6a7ce6d5b9bb542
Sari Hammad <sari_hammad@hotmail.com>
2022-10-19 01:17:31 -0400

    fixed interface

--- router/sr_arpcache.c
+++ router/sr_arpcache.c
@@ -23,7 +23,6 @@ void handle_arpreq(struct sr_instance *sr, struct sr_arpreq *arpreq) {
 
             /* Iterate through all the packets waiting on the ARP request */     
             while (pckt) {
-
                 /* Send ICMP host unreachable to source address of all packets waiting */
                 struct sr_if* iface = sr_get_interface(sr, pckt->iface);
                 handle_icmp_request(sr, pckt->buf, pckt->len, 3, 1, iface);
